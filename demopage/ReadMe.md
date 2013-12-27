2c37f42b24ef26bb76c3c7fb28cbba2b028276a8
徐凤超 <xufengchao@antiy.com>
2013-12-27 10:07:05 +0800

    init demopage

---
+++ demopage/ReadMe.md
@@ -0,0 +1,8 @@
+使用 WIN 8 开始菜单风格创建平时Demo的整体的链接页面
+
+访问地址 ： http://10.14.14.14/
+
+添加修改链接地址
+    在 js/linkData.js 中操作 具体格式文件里有注释
+
+![image](http://10.255.80.48/xufengchao/demo/blob/master/img/index.png) 
---
+++ demopage/js/linkData.js
@@ -0,0 +1,85 @@
+/*
+        名称    链接地址     images ( 310 * 150 double属性将无效 ) 没有将被填充随机颜色 卡片大小( 横向double 纵向 double-vertical )
+*/
+
+var linkDatas = [
+{
+    'group-name': '疫情监控',
+    'group-info': 'WebGL',
+    'group-data': [
+        {'name' : '疫情趋势 - 世界', 'href': 'http://10.14.14.14/virus_globe/index.html', 'images': 'globl.jpg','color': '','card': 'double'},
+        {'name' : '疫情趋势 - 中国', 'href': 'http://10.14.14.14/chinaTrend/china_trend.html', 'images': 'newui.png' ,'color': '','card': 'double'},
+        {'name' : '疫情监控 - 世界', 'href': 'http://10.255.80.5/world.html', 'images': 'dpworld.png' ,'color': '','card': 'double'},
+        {'name' : '疫情监控 - 中国', 'href': 'http://10.255.80.5/hlj.html', 'images': 'dphlj.png','color': '','card': 'double'},
+        {'name' : '新版疫情趋势 - 世界', 'href': 'http://10.14.14.14/newdaping/world.html', 'images': 'newdpworld.png' ,'color': '','card': 'double '},
+        {'name' : '新版疫情监控 - 中国', 'href': 'http://10.14.14.14/newdpchina/blue.html', 'images': 'newdpchina.png','color': '','card': 'double'}
+        
+    ]
+},
+{
+    'group-name': '安全事件展示',
+    'group-info': '',
+    'group-data': [
+        
+        {'name' : '失窃密主动监管平台', 'href': 'http://10.14.14.14/earthgf/index.html', 'images': '','color': '','card': 'double'},
+        {'name' : '地  球', 'href': 'http://10.14.14.14/earth/index.html', 'images': 'earth.png','color': '','card': 'double double-vertical'},
+        
+    ]
+},
+
+{
+    'group-name': 'WB',
+    'group-info': '',
+    'group-data': [
+        {'name' : '攻防演习展示', 'href': 'http://10.14.14.14/staticwb/index_timer_static.html', 'images': 'wb.png' ,'color': '','card': 'double'}, 
+        {'name' : '网络拓扑结构展示', 'href': 'http://10.14.14.14/topos/pages/index/index.html', 'images': 'topos_index.png' ,'color': '','card': 'double'},
+        {'name' : '设备监控展示', 'href': 'http://10.14.14.14/topos/pages/platform_topo/platform_topo.html', 'images': 'platform_topo.png' ,'color': '','card': 'double'}
+    ]
+},
+
+{
+    'group-name': '可视化',
+    'group-info': '',
+    'group-data': [
+        {'name' : '恶意代码可视化', 'href': 'http://10.14.14.14/virusbk/index.html', 'images': 'virusbk.png','color': '','card': 'double'},
+        {'name' : '拓扑 - 星云', 'href': 'http://10.14.14.14/starmap/index.html', 'images': 'startmap.png','color': '','card': 'double'},
+        {'name' : '沙  盘', 'href': 'http://10.14.14.14/t_platform/t_platform.html', 'images': 'spcn.png','color': '','card': 'double'},
+        {'name' : '中水系统', 'href': 'http://10.14.14.14/zhongshui/webgl_loader_json_objconverter.html', 'images': '','color': '','card': 'double'}
+        // {'name' : '团队建设', 'href': 'http://10.14.14.14/person/person.html', 'images': 'person.png','color': '','card': 'double'},
+        
+    ]
+},
+
+{
+    'group-name': 'X系统展示',
+    'group-info': '',
+    'group-data': [
+        {'name' : 'X系统部署运行展示v0.1', 'href': 'http://10.14.14.14/xietong_scene_min/index.html', 'images': 'xitong_scene_min.png','color': '','card': 'double'},
+        {'name' : 'X系统地理分布展示', 'href': 'http://10.14.14.14/c_monitor/index.html', 'images': 'c_monitor.png' ,'color': '','card': 'double '},
+        {'name' : 'X系统架构展示v0.2', 'href': 'http://10.14.14.14/xitong_scene/index.html', 'images': 'xitong_scene.png' ,'color': '','card': 'double '}
+    ]
+},
+
+
+
+{
+    'group-name': '图表设计',
+    'group-info': '',
+    'group-data': [
+        {'name' : '三维图表', 'href': 'http://10.14.14.14/threechart/index.html', 'images': '3dchart.png','color': '','card': 'double'},
+        {'name' : '趋势柱图', 'href': 'http://10.14.14.14/custom-bar/index.html', 'images': 'cbar.png' ,'color': '','card': 'double'},
+        {'name' : 'AVL SDK 流程图', 'href': 'http://10.14.14.14/avl_web_demo/newpage.html', 'images': '' ,'color': '','card': 'double'},
+    ]
+},
+
+{
+    'group-name': 'Others',
+    'group-info': '',
+    'group-data': [
+        
+        {'name' : 'Strut', 'href': 'http://10.14.14.14/strut/index.html', 'images': '' ,'color': 'red','card': ''},
+        {'name' : 'GitLab', 'href': 'http://10.255.80.48', 'images': '' ,'color': 'blueDark','card': ''},
+
+    ]
+}
+];
